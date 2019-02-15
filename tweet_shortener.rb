8bcb38c6b41ea6eee139b69a7654aac160092b4f
Trey <tbeau5595@hotmail.com>
2019-02-15 02:09:19 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,14 +1,14 @@
 def dictionary
   substitute = {
     "Hello" => "hi"
-    "to=>
-    =>
-    =>
-    =>
-    =>
-    =>
-    =>
-    =>
-    =>
+    "to" => "2"
+    "two" => "2"
+    "too" => "2"
+    "for" => "4"
+    "four" => "4"
+    "be" => "b"
+    "you" => "u"
+    "at" => "@"
+    "and" => "&"
     
   }
